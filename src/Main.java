45dc8b1d251182d6df42e3a7bbb3319f12e50ccc
AlexisKH <n01653801@humber.ca>
2026-07-20 20:23:41 -0400

    Completed Lab 8

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,43 @@ import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(evaluatePostfix("1 2 + 3 *"));
     }
 
     public static double evaluatePostfix(String expr) {
         // TODO: Implement this method
-        return 0.0;
+        Stack<Double> stack = new Stack<>();
+
+        String[] tokens = expr.split(" ");
+
+        for (String token : tokens) {
+            if (token.equals("+") ||
+                    token.equals("-") ||
+                    token.equals("*") ||
+                    token.equals("/")) {
+
+                double right = stack.pop();
+                double left = stack.pop();
+
+                switch (token) {
+                    case "+":
+                        stack.push(left + right);
+                        break;
+                    case "-":
+                        stack.push(left - right);
+                        break;
+                    case "*":
+                        stack.push(left * right);
+                        break;
+                    case "/":
+                        stack.push(left / right);
+                        break;
+                }
+            } else {
+                stack.push(Double.parseDouble(token));
+            }
+        }
+
+        return stack.pop();
     }
 }
